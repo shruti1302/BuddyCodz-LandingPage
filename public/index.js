3431d337adb747054af2e4c84d2221a9cbe477fe
Shruti <agarwal.shruti1302@gmail.com>
2020-11-06 00:26:20 +0530

    First Commit

---
+++ public/index.js
@@ -0,0 +1,61 @@
+particlesJS('particles-js', {
+  particles: {
+    number: { value: 160, density: { enable: true, value_area: 800 } },
+    color: { value: '#ffffff' },
+    shape: {
+      type: 'circle',
+      stroke: { width: 0, color: '#000000' },
+      polygon: { nb_sides: 5 },
+      image: { src: 'img/github.svg', width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: 'none',
+      random: true,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: 'window',
+    events: {
+      onhover: { enable: true, mode: 'repulse' },
+      onclick: { enable: false, mode: 'remove' },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 0, line_linked: { opacity: 1 } },
+      bubble: {
+        distance: 1500,
+        size: 48.72463273808071,
+        duration: 1.7865698670629593,
+        opacity: 0.7308694910712106,
+        speed: 3,
+      },
+      repulse: { distance: 97.44926547616141, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
